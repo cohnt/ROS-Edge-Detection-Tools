b26614a5da5a915391f55a8db837a95ca7577005
Thomas Cohn <cohnt@umich.edu>
2018-05-24 14:41:31 -0400

    Remove some unused, commented-out code.

--- detect_handles.py
+++ detect_handles.py
@@ -17,24 +17,10 @@ cell_size = (8, 8)
 window_size = (18, 12)
 
 image = imread(fname)
-
 fd, hog_image = hog(rgb2gray(image), orientations=8, pixels_per_cell=cell_size, cells_per_block=(1, 1), visualise=True, feature_vector=False, block_norm='L2')
-
-# print fd
-
-# fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 4), sharex=True, sharey=True)
+hog_image_rescaled = exposure.rescale_intensity(hog_image, in_range=(0, 10))
 fig, ax = plt.subplots()
 
-# ax1.axis('off')
-# ax1.imshow(image, cmap=plt.cm.gray)
-# ax1.set_title('Input image')
-
-# Rescale histogram for better display
-hog_image_rescaled = exposure.rescale_intensity(hog_image, in_range=(0, 10))
-
-# ax2.axis('off')
-# ax2.imshow(hog_image_rescaled, cmap=plt.cm.gray)
-# ax2.set_title('Histogram of Oriented Gradients')
 ax.axis('off')
 ax.imshow(hog_image_rescaled, cmap=plt.cm.gray)
 
