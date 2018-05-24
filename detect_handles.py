a4583af35a68d7ebf8e62d03ba9fd0c8b6859d58
Thomas Cohn <cohnt@umich.edu>
2018-05-24 14:54:32 -0400

    Switch to using a main function, for convenience and organization.

--- detect_handles.py
+++ detect_handles.py
@@ -16,14 +16,6 @@ fname = "camera_image.jpeg"
 cell_size = (8, 8)
 window_size = (18, 12)
 
-image = imread(fname)
-fd, hog_image = hog(rgb2gray(image), orientations=8, pixels_per_cell=cell_size, cells_per_block=(1, 1), visualise=True, feature_vector=False, block_norm='L2')
-hog_image_rescaled = exposure.rescale_intensity(hog_image, in_range=(0, 10))
-fig, ax = plt.subplots()
-
-ax.axis('off')
-ax.imshow(hog_image_rescaled, cmap=plt.cm.gray)
-
 class WindowIndicator(object):
 	def __init__(self, ax):
 		self.ax = ax
@@ -43,9 +35,6 @@ class WindowIndicator(object):
 		self.hl2.set_ydata(y+(cell_size[1]*window_size[1])-(y % cell_size[1]))
 		self.ax.figure.canvas.draw_idle()
 
-cursor = WindowIndicator(ax)
-cid =  plt.connect('motion_notify_event', cursor.mouse_move)
-
 def onclick(event):
 	if ax.in_axes(event):
 		# Transform the event from display to axes coordinates
@@ -61,9 +50,23 @@ def onclick(event):
 	else:
 		print("Outside of figure!")
 
-cid = fig.canvas.mpl_connect('button_press_event', onclick)
+def main():
+	image = imread(fname)
+	fd, hog_image = hog(rgb2gray(image), orientations=8, pixels_per_cell=cell_size, cells_per_block=(1, 1), visualise=True, feature_vector=False, block_norm='L2')
+	hog_image_rescaled = exposure.rescale_intensity(hog_image, in_range=(0, 10))
+	fig, ax = plt.subplots()
 
-mng = plt.get_current_fig_manager()
-mng.resize(*mng.window.maxsize())
+	ax.axis('off')
+	ax.imshow(hog_image_rescaled, cmap=plt.cm.gray)
 
-plt.show()
+	cursor = WindowIndicator(ax)
+	cid =  plt.connect('motion_notify_event', cursor.mouse_move)
+
+	mng = plt.get_current_fig_manager()
+	mng.resize(*mng.window.maxsize())
+
+	plt.show()
+
+
+if __name__ == '__main__':
+	main()
